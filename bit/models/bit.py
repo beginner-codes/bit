421fa455ddaf09315e18d90c9be1e00813ee61fb
ZechCodes <hi@zech.codes>
2022-05-04 17:27:41 -0400

    Add archive column to bits

--- bit/models/bit.py
+++ bit/models/bit.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Unicode
+from sqlalchemy import Column, Integer, Boolean
 from bit.models.base import Base
 
 
@@ -6,3 +6,4 @@ class Bit(Base):
     __tablename__ = "Bit"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, nullable=False)
+    archived = Column(Boolean, default=False)
