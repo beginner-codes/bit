e3f7717518e6de7cf1d791a842f55f0425029683
ZechCodes <hi@zech.codes>
2022-05-04 17:29:42 -0400

    Add the audit model

---
+++ bit/models/audit.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Unicode, DateTime
+from sqlalchemy.sql import func
+from bit.models.base import Base
+
+
+class Audit(Base):
+    __tablename__ = "Audit"
+    id = Column(Integer, primary_key=True)
+    action = Column(Unicode(256), nullable=False)
+    details = Column(Unicode(1024), nullable=False)
+    bit_id = Column(Integer, nullable=True)
+    code_id = Column(Integer, nullable=True)
+    file_id = Column(Integer, nullable=True)
+    user_id = Column(Integer, nullable=True)
+    when = Column(DateTime, nullable=False, server_default=func.now)
