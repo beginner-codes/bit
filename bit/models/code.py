a2eb9c074c563bc3505692255be503304c3cec3c
ZechCodes <hi@zech.codes>
2022-05-04 17:22:47 -0400

    Add code model

---
+++ bit/models/code.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, Unicode, DateTime
+from sqlalchemy.sql import func
+from bit.models.base import Base
+
+
+class Code(Base):
+    __tablename__ = "Code"
+    id = Column(Integer, primary_key=True)
+    code = Column(Unicode(2**14), nullable=False)
+    file_id = Column(Integer, nullable=False)
+    created = Column(DateTime, nullable=False, server_default=func.now)
