0cae509a88b0fe45bf97b67271110ef50ef4591d
ZechCodes <hi@zech.codes>
2022-06-15 19:08:35 -0400

    Update bootstrap to use new endpoint locations

--- bit/__main__.py
+++ bit/__main__.py
@@ -1,16 +1,15 @@
 import logging
-import uvicorn
 import os
 
+import uvicorn
 
 debug = os.getenv("DEBUG", False) == "True"
 logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
 logging.info(f'Debugging={os.getenv("DEBUG", False)!r}')
 
-
 uvicorn.run(
-    "bit.site:app",
+    "bit.endpoints.api:api_app",
     port=8000,
     log_level="debug" if debug else "info",
-    reload=debug
+    reload=debug,
 )
